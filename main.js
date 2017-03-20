3a472ee7755d36804fa5dfeca0bee43631a98183
GoForMarty <marty.stepien@gmail.com>
2017-03-20 11:54:43 +0100

    checkGuess function added

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 //alert('JS is here!');
+var randomNumber = Math.floor(Math.random()*100)+1;
+var guesses = document.querySelector('.guesses');
+var lastResult = document.querySelector('.lastResult');
+var lowOrHigh = document.querySelector('.lowOrHigh');
+var guessSubmit = document.querySelector('.guessSubmit');
+var guessField = document.querySelector('.guessField');
+var guessCount = 1;
+var resetButton;
+
+function checkGuess() {
+	var userGuess = Number(guessField.value);
+	if(guessCount===1) {
+		guess.textContent = 'Previous guesses: ';
+	}
+	guesses.textContent = userGuess + ' ';
+
+	if (userGuess===randomNumber) {
+		lastResult.textContent = 'Congratulations! You got it right!';
+		lastResult.style.backgrounColor = 'green';
+		lowOrHigh.textContent = '';
+		setGameOver();
+	} else if (count === 10) {
+		lastResult.textContent = 'GAME OVER!';
+		setGameOver();
+	} else {
+		lastResult.textContent = 'Wrong!';
+		lastResult.style.backgrounColor = 'red';
+		if (userGuess < randomNumber) {
+			lowOrHigh.textContent = 'My number is higher!';
+		} else if (userGuess > randomNumber) {
+			lowOrHigh.textContent = 'My number is lower!';
+		}
+	}
+	guessCount++;
+	guessField.value= '';
+	guessField.focus();
+}
